51dbaa1cbd831669b2f0228dff30836b34a7c8cc
mehedi emon <mehediemon7@gmail.com>
2020-10-09 21:55:38 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return ( <div className="banner">
+    </div> );
+}
+ 
+export default Header;
